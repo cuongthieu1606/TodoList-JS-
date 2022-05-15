5ad9953943d95906b9c4d2f0fa22e98532f6e196
cuongthieu1606 <thieutrancuong02@gmail.com>
2022-05-15 10:58:35 +0700

    todolist

---
+++ script.js
@@ -0,0 +1,106 @@
+const taskInput = document.querySelector(".task-input input");
+filters = document.querySelectorAll(".filters span");
+clearAll = document.querySelector(".clear-btn");
+const taskBox = document.querySelector(".task-box");
+
+let editId;
+let isEditTask = false;
+
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    btn.classList.add("active");
+    showTodo(btn.id);
+  });
+});
+
+function showTodo(filter) {
+  let li = "";
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let isCompleted = todo.status == "completed" ? "checked" : "";
+      if (filter == todo.status || filter == "all") {
+        li += `<li class="task">
+                  <label for="${id}">
+                    <input onclick={updateStatus(this)} type="checkbox" id="${id}" ${isCompleted} />
+                    <p class="${isCompleted}">${todo.name}</p>
+                  </label>
+                  <div class="settings">
+                    <i onclick={showMenu(this)} class="bx bx-dots-horizontal-rounded"></i>
+                    <ul class="task-menu">
+                      <li onclick="editTask(${id}, '${todo.name}')"><i class="bx bx-pencil"></i>Edit</li>
+                      <li onclick="deleteTask(${id})"><i class="bx bx-trash"></i>Delete</li>
+                    </ul>
+                  </div>
+                </li>`;
+      }
+    });
+  }
+  taskBox.innerHTML = li || `<span>You dont't have talk here</span>`;
+}
+showTodo("all");
+
+function showMenu(selectedTask) {
+  let taskMenu = selectedTask.parentElement.lastElementChild;
+  taskMenu.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != selectedTask) {
+      taskMenu.classList.remove("show");
+    }
+  });
+}
+
+function editTask(taskId, taskName) {
+  editId = taskId;
+  isEditTask = true;
+  taskInput.value = taskName;
+}
+
+function deleteTask(deleteId) {
+  todos.splice(deleteId, 1);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
+}
+
+clearAll.addEventListener("click", () => {
+  todos.splice(0, todos.length);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo("all");
+});
+
+function updateStatus(selectedTask) {
+  let taskName = selectedTask.parentElement.lastElementChild;
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    todos[selectedTask.id].status = "completed";
+  } else {
+    taskName.classList.remove("checked");
+    todos[selectedTask.id].status = "pending";
+  }
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+taskInput.addEventListener("keyup", (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key == "Enter" && userTask) {
+    if (!isEditTask) {
+      // Getting localStorege todo-list
+      if (!todos) {
+        todos = [];
+      }
+      let taskInfo = {
+        name: userTask,
+        status: "pending",
+      };
+      todos.push(taskInfo);
+    } else {
+      isEditTask = false;
+      todos[editId].name = userTask;
+    }
+    taskInput.value = "";
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo("all");
+  }
+});
